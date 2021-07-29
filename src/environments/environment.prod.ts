7b4d3e7f3f71aa1f7a95873aaea299b0da19aba6
HenKyubi666 <jhonnino@unisangil.edu.co>
2021-07-29 07:53:49 -0500

    avances en casa

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDC26vWUgAPwtbPYoCqbGD1mwrRFyKzr3E",
+    authDomain: "empleados-f6d92.firebaseapp.com",
+    projectId: "empleados-f6d92",
+    storageBucket: "empleados-f6d92.appspot.com",
+    messagingSenderId: "220571311630",
+    appId: "1:220571311630:web:d57cc5deeae7d6d34aa65f"
+  }
 };
